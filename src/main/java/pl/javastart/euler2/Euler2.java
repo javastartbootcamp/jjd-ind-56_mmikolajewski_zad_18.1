399eba5642524c3434fa861cf6a5f61a8450bbf1
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-03-15 19:58:02 +0000

    rozwiązanie

--- src/main/java/pl/javastart/euler2/Euler2.java
+++ src/main/java/pl/javastart/euler2/Euler2.java
@@ -1,8 +1,27 @@
 package pl.javastart.euler2;
 
 public class Euler2 {
-
     public static void main(String[] args) {
+        Euler2 euler2 = new Euler2();
+        int sum = euler2.countFibonacci(4000000);
+        System.out.println("\nsuma elementów parzystych " + sum);
+    }
 
+    public int countFibonacci(int limit) {
+        int firstTerm = 0;
+        int secondsTerm = 1;
+        int sum = 0;
+
+        while (firstTerm <= limit) {
+            if (firstTerm % 2 == 0) {
+                sum += firstTerm;
+            }
+            System.out.print(firstTerm + ", ");
+
+            int nextTerm = firstTerm + secondsTerm;
+            firstTerm = secondsTerm;
+            secondsTerm = nextTerm;
+        }
+        return sum;
     }
 }
